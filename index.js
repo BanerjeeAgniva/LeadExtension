0c3ed439e561c7e38e7455390255ce42f43abf37
BanerjeeAgniva <90470431+BanerjeeAgniva@users.noreply.github.com>
2024-10-02 16:30:54 +0530

    Tab button for  index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -23,6 +24,14 @@ function render(leads) {
     }
     ulEl.innerHTML = listItems
 }
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
