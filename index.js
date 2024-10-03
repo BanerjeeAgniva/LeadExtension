6440ca3b885d28a6062ca8a70dda8d0983ad4802
Agniva Banerjee <agniva.banerjee@harness.io>
2024-10-03 11:57:57 +0530

    just some comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let myLeads = []
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el") // read or edit an HTML element.
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
@@ -13,8 +13,8 @@ if (leadsFromLocalStorage) {
 
 function render(leads) {
     let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+    for (let i = 0; i < leads.length; i++) { // parameterized string!!
+        listItems += `  
             <li>
                 <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
@@ -26,8 +26,11 @@ function render(leads) {
 }
 
 tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){ // active --> activeTab || currentWindow --> activeWindow 
+        myLeads.push(tabs[0].url) // The chrome.tabs.query() function retrieves an array of tab objects that match the specified query. 
+        //the query filters the active tab in the current window. 
+        //Since the query is restricted to the current active tab, tabs[0] refers to the first (and only) result in this array—representing the active tab.
+        //tabs[0].url: This accesses the url property of the active tab. The url is the current web address (e.g., "https://example.com") of the tab.
         localStorage.setItem("myLeads", JSON.stringify(myLeads) )
         render(myLeads)
     })
@@ -41,7 +44,7 @@ deleteBtn.addEventListener("dblclick", function() {
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    inputEl.value = ""
+    inputEl.value = "" 
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
