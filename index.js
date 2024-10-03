f3fcf8d3f347021d01254a7fce822b34872e8879
BanerjeeAgniva <90470431+BanerjeeAgniva@users.noreply.github.com>
2024-10-03 21:49:07 +0530

    Minor variable name changes Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let myLeads = []
-const inputEl = document.getElementById("input-el") // read or edit an HTML element.
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
+const input_box = document.getElementById("input-el") // read or edit an HTML element.
+const input_button  = document.getElementById("input-btn")
+const unordered_list = document.getElementById("ul-el")
+const delete_button = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+const tab_button = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -22,10 +22,10 @@ function render(leads) {
             </li>
         `
     }
-    ulEl.innerHTML = listItems
+    unordered_list.innerHTML = listItems
 }
 
-tabBtn.addEventListener("click", function(){    
+tab_button.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){ // active --> activeTab || currentWindow --> activeWindow 
         myLeads.push(tabs[0].url) // The chrome.tabs.query() function retrieves an array of tab objects that match the specified query. 
         //the query filters the active tab in the current window. 
@@ -36,15 +36,15 @@ tabBtn.addEventListener("click", function(){
     })
 })
 
-deleteBtn.addEventListener("dblclick", function() {
+delete_button.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = "" 
+input_button .addEventListener("click", function() {
+    myLeads.push(input_box.value)
+    input_box.value = "" 
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
